129565173e34a84c84047d15f52ec9ffc24c7671
zyn10 <zainandcs@gmail.com>
2023-08-11 22:48:25 +0500

    Problems Removed

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:petbook_admin/alerts/local_alerts.dart';
 import 'package:petbook_admin/screens/splash_screen.dart';
 
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  //print("Handling a background message: ${message.messageId}");
-}
-
 Future<void> main() async {
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    //print('Got a message whilst in the foreground!');
-    //print('Message data: ${message.data}');
-
-    if (message.notification != null) {
-      //print('Message also contained a notification: ${message.notification}');
-    }
+    if (message.notification != null) {}
   });
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -32,11 +23,9 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
